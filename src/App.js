4ae06eea0057f20a16ef9da222757b5305f00d94
SyedRahadHossain <syedhossainibnali@gmail.com>
2022-03-29 17:35:36 +0600

    primarily component structure done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import Banner from './components/Banner/Banner';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import Services from './components/Services/Services';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AuthProvider from './contexts/AuthProvider';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Notfound from './components/Notfound/Notfound';
+import Login from './components/Login/Login';
+import PrivateRoute from './PrivateRoute/PrivateRoute';
+import ServiceDetails from './components/ServiceDetails/ServiceDetails';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <AuthProvider>
+        <Router>
+          <Header></Header>
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route path="/home">
+              <Home></Home>
+            </Route>
+            <Route path="/about">
+              <About></About>
+            </Route>
+            
+            <Route path="/login">
+              <Login></Login>
+            </Route>
+            <PrivateRoute path="/servicedetails/:serviceId">
+              <ServiceDetails></ServiceDetails>
+            </PrivateRoute>
+            <Route path="*">
+              <Notfound></Notfound>
+            </Route>
+          </Switch>
+          <Footer></Footer>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
---
+++ src/components/Notfound/Notfound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Notfound = () => {
+  return (
+    <div>
+      {/* <img className="my-4" src={notfound} alt="" /> */}
+      <h2>not found</h2>
+    </div>
+  );
+};
+
+export default Notfound;
